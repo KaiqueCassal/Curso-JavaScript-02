148fd07e6a15f422d82f3697421937bdcde060b0
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-25 20:56:31 -0300

    aula007
    Data Types.

---
+++ aula/007-dataTypes/app.js
@@ -0,0 +1,13 @@
+//Data Types
+
+/*
+Number = 10, -10,  5.5 (integer - float) [Infinity, - Infinity e NaN]
+BigInt = 9999999999999999999999n, -999999999999999n
+String = 'a', "B", `c ${variavel}`
+Boolean = true, false, 0, 1
+Null value = null
+Undefined value = não definido
+
+Objetos 
+Simbolos
+*/
