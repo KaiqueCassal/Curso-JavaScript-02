728724ee61c757fe300c52613aaf93ca314650a3
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-25 18:34:23 -0300

    ex
    Exercícios.

---
+++ exercicio/001/app.js
@@ -0,0 +1,4 @@
+let A = B = C = 100, D = 1000
+
+console.clear()
+console.log(B)
---
+++ exercicio/002/app.js
@@ -0,0 +1,7 @@
+let A = 10, B = 20, C = A
+console.clear()
+console.log(C)
+let t = A
+A = B
+B = t
+console.log(A, B)
