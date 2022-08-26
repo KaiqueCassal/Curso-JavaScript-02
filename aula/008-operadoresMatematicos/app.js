ebae2acfdfdfedabe156e51756f8acdf991a8083
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-26 17:02:25 -0300

    aula008
    Operadores Matemáticos.

---
+++ aula/008-operadoresMatematicos/app.js
@@ -0,0 +1,20 @@
+let valor1 = 10
+valor1 = -valor1
+
+let valor2 = 10, valor3 = 20
+let resultado = valor2 + valor3
+
+a + b //adição [concatenação]
+a - b //subtração
+a * b //multiplicação
+a / b //divisão
+a % b //resto da divisão
+a ** b//potenciação
+
++true //1
++false //0
+
+let valor_a = '10', valor_b = '20'
+
++valor_a + +valor_b //30
+Number(valor_a.value) + Number(valor_b.value) //30
