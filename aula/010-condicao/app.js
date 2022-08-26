4c1810a14bba93a15fb2cedebbbf94d0793e1bae
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-26 17:28:33 -0300

    aula010
    Condições.

---
+++ aula/010-condicao/app.js
@@ -0,0 +1,27 @@
+let valor_a = 10, valor_b = 20
+
+if(valor_a < valor_b){
+    console.log('ok')
+}
+
+//if(valor_a < valor_b)console.log('ok')
+
+if(condição){
+    true
+}else{
+    false
+}
+
+if(condição){
+    true
+}else if(condição){
+    true
+}else if(condição){
+    true
+}else{
+    false
+}
+
+let nome = 'kaiq'
+let verificacao = nome == 'kaiq' ? 'sim' : 'não'
+console.log(verificacao)
