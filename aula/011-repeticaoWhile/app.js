91ae1d69432479f55c49ae46784d3329c4c3466c
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-30 16:31:04 -0300

    aula011
    Repetições While e Do While.

---
+++ aula/011-repeticaoWhile/app.js
@@ -0,0 +1,26 @@
+while(condição){
+    // código
+}
+
+let n = 1
+while(n <= 10){
+    console.log(n)
+    n++ //n = n + 1
+}
+
+/*
+let n = 1
+while(n <= 10) console.log(n++)
+*/
+
+let num = 10
+while(num >= 1){
+    console.log(num)
+    num-- 
+}
+
+let numero = 1
+do{
+    console.log(numero)
+    numero++
+}while(numero <= 10)
