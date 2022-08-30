2a8744d942c999ac4255028855996206d7a6b06e
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-30 16:57:53 -0300

    aula014
    Function.

---
+++ aula/014-function/app.js
@@ -0,0 +1,11 @@
+//function nome_da_funcao(argumento){
+    //código da função
+//}
+//nome_da_funcao(x)
+
+function adicionar(a=0, b=0){
+    let r = a + b
+    return r
+}
+
+console.log(adicionar(10, 30))
