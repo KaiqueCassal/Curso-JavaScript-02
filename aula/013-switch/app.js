99f62ae2975c2b68252194737b100f007e465afa
KaiqCassal <kaiquemiguelcassal20@gmail.com>
2022-08-30 16:46:55 -0300

    aula013
    Switch.

---
+++ aula/013-switch/app.js
@@ -0,0 +1,14 @@
+let v = 3
+switch(v){
+    case 1:
+        console.log('Valor é igual a 1')
+        break
+    case 2:
+        console.log('Valor é igual a 2')
+        break
+    case 3:
+        console.log('Valor é igual a 3')
+        break
+    default:
+        console.log('Nada')
+}
